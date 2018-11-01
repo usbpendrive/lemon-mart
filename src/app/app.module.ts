14baf3fca56f044cd058973e96061b1e2de8327c
Fernando <ferry.fernando@gmail.com>
2018-11-01 16:00:08 +0700

    Home component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { MaterialModule } from './material.module'
 import { ManagerModule } from './manager/manager.module'
 import { InventoryModule } from './inventory/inventory.module'
 import { PosModule } from './pos/pos.module'
-import { UserModule } from './user/user.module'
+import { UserModule } from './user/user.module';
+import { HomeComponent } from './home/home.component'
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
