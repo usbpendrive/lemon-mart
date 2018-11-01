d6b5fca62c07b5961287c6519c8c0f2be4ab9e0d
Fernando <ferry.fernando@gmail.com>
2018-11-01 15:56:33 +0700

    Material themes.

---
+++ src/app/material.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core'
+import { MatButtonModule, MatIconModule, MatToolbarModule } from '@angular/material'
+
+@NgModule({
+  imports: [MatButtonModule, MatToolbarModule, MatIconModule],
+  exports: [MatButtonModule, MatToolbarModule, MatIconModule]
+})
+export class MaterialModule {}
