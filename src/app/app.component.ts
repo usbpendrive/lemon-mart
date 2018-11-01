ec19321f0181518270809663893ce7a589b8679d
Fernando <ferry.fernando@gmail.com>
2018-11-02 06:23:30 +0700

    Router outlet.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,13 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template: `
+    <mat-toolbar color="primary">
+      <a mat-button routerLink="/home"><h1>LemonMart</h1></a>
+    </mat-toolbar>
+    <router-outlet></router-outlet>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
