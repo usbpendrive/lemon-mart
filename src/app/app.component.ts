09fb7deda04b62e8ab90a5288df3fa7cffb91332
Fernando <ferry.fernando@gmail.com>
2018-11-02 08:24:59 +0700

    User module.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,19 @@ import { MatIconRegistry } from '@angular/material'
   selector: 'app-root',
   // templateUrl: './app.component.html',
   template: `
-    <mat-toolbar color="primary">
+    <mat-toolbar color="primary" fxLayoutGap="8px">
       <button mat-icon-button><mat-icon>menu</mat-icon></button>
       <mat-icon svgIcon="lemon"></mat-icon>
       <a mat-button routerLink="/home"><h1>LemonMart</h1></a>
       <span class="flex-spacer"></span>
-      <button mat-icon-button><mat-icon>account_circle</mat-icon></button>
-      <button mat-icon-button><mat-icon>lock_open</mat-icon></button>
+      <button mat-mini-fab routerLink="/user/profile" matTooltip="Profile"
+        aria-label="User Profile">
+        <mat-icon>account_circle</mat-icon>
+      </button>
+      <button mat-mini-fab routerLink="/user/logout" matTooltip="Logout"
+        aria-label="logout">
+        <mat-icon>lock_open</mat-icon>
+      </button>
     </mat-toolbar>
     <router-outlet></router-outlet>
   `,
