a078d1fe881158ec1fc24b9490bcfc522302ede7
Fernando <ferry.fernando@gmail.com>
2018-11-02 09:03:10 +0700

    Common testing providers.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,10 +2,23 @@ import { TestBed, async } from '@angular/core/testing'
 import { RouterTestingModule } from '@angular/router/testing'
 import { AppComponent } from './app.component'
 
+import {
+  commonTestingModules, commonTestingProviders,
+  MatIconRegistryFake, DomSanitizerFake, ObservableMediaFake
+} from './common/common.testing'
+import { ObservableMedia } from '@angular/flex-layout'
+import { MatIconRegistry } from '@angular/material'
+import { DomSanitizer } from '@angular/platform-browser'
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: commonTestingModules,
+      providers: commonTestingProviders.concat([
+        { provide: ObservableMedia, useClass: ObservableMediaFake },
+        { provide: MatIconRegistry, useClass: MatIconRegistryFake },
+        { provide: DomSanitizer, useClass: DomSanitizerFake }
+      ]),
       declarations: [AppComponent]
     }).compileComponents()
   }))
@@ -19,7 +32,7 @@ describe('AppComponent', () => {
   it(`should have as title 'lemon-mart'`, () => {
     const fixture = TestBed.createComponent(AppComponent)
     const app = fixture.debugElement.componentInstance
-    expect(app.title).toEqual('lemon-mart')
+    expect(app.title).toEqual('LemonMart')
   })
 
   it('should render title in a h1 tag', () => {
